7a7df2282249a2bed695c69711d441f75c7c9fab
Lennincitalan <lennin.pumas@gmail.com>
2021-12-16 13:26:01 -0600

    pruebas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Carrera;
+use App\Models\Empleado;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,72 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $empleado = new Empleado();
+        $empleado->nombre = "Informatico";
+        $empleado->apellido_paterno = "Apellido";
+        $empleado->apellido_materno= "Apellido";
+        $empleado->correo="vlc960157@gmail.com";
+        $empleado->password="1234";
+        $empleado->rol="Informático";
+        $empleado->puesto="puesto 1";
+        $empleado->quien_revisa="ggg";
+        $empleado->confirmed="1";
+        $empleado->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera2 = new Carrera();
+        // $carrera2->name="Ingen. Mecánica";
+        // $carrera2->logo="storage/logos/ing_mecanica.png";
+        // $carrera2->save();
+
+        // $carrera3 = new Carrera();
+        // $carrera3->name="Ingen. Mecánica";
+        // $carrera3->logo="storage/logos/ing_mecanica.png";
+        // $carrera3->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
+
+        // $carrera = new Carrera();
+        // $carrera->name="Ingen. Mecánica";
+        // $carrera->logo="storage/logos/ing_mecanica.png";
+        // $carrera->save();
     }
 }
